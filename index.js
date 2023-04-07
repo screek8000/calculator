4ad7bf28fe4852fc936cc92ecbd65b655d79b075
Trynax <kandoso111@gmail.com>
2023-04-07 06:52:15 +0100

    progress

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let display = document.querySelector('.display')
 let clear = document.querySelector('#clear')
 let operator;
 let secondNumber;
-let getNum = document.querySelector('.btn')
-firstNumber = parseInt(getNum.textContent) 
-secondNumber = parseInt(getNum.textContent) 
+let getElement = document.querySelectorAll('#click')
+firstNumber = parseInt(getElement.textContent) 
+secondNumber = parseInt(getElement.textContent) 
 const sum = ()=>{
   return firstNumber + secondNumber;
 }
@@ -23,3 +23,13 @@ const operate = () =>{
 clear.addEventListener('click', function () {
    display.textContent=""
 })
+
+getElement.forEach(function (element) {
+  element.addEventListener('click', function(){
+    display.textContent += parseInt(element.textContent)
+    firstNumber = display.textContent
+    
+  })
+  
+})
+console.log(firstNumber);
