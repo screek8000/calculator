b04c0c7ba702a863acd0d08c62b466876f8d258d
Trynax <kandoso111@gmail.com>
2023-04-11 11:21:20 +0100

    set up complete

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 let firstNumber;
 let display = document.querySelector('.display')
+display.textContent="";
 let clear = document.querySelector('#clear')
 let operators = document.querySelectorAll('.operator')
 let operator;
+let results;
 let secondNumber;
 let getElement = document.querySelectorAll('#click')
+let operatorClicked = false;
 
 const sum = ()=>{
+  results = Number(firstNumber + secondNumber); 
   return firstNumber + secondNumber;
+  
 }
 const substract = ()=>{
+
+  results =  Number(firstNumber - secondNumber);
   return firstNumber - secondNumber;
+
 }
 const divide = ()=>{
+  results = Number(firstNumber / secondNumber);
   return firstNumber / secondNumber;
 }
 const multiply = ()=>{
-  return firstNumber * secondNumber
+  results = Number(firstNumber * secondNumber);
+  return firstNumber * secondNumber;
 }
 const operate = () =>{
+
   if (operator=='+'){
     sum()
+    
   }
  else if (operator=='-'){
     substract()
@@ -31,30 +43,41 @@ const operate = () =>{
  else if (operator=='*'){
     multiply()
   }
+  firstNumber=results;
+  display.textContent=results;
+  
   
 }
 clear.addEventListener('click', function () {
+  firstNumber=undefined;
+  secondNumber=undefined;
+  operator=undefined;
+  operatorClicked=false
    display.textContent=""
 })
 
 getElement.forEach(function (element) {
   element.addEventListener('click', function(){
-    display.textContent += parseInt(element.textContent)
-    firstNumber =display.textContent
+    if(!operatorClicked){
+      display.textContent += parseInt(element.textContent)
+      firstNumber = Number(display.textContent)
+    }else if(operatorClicked){
+      display.textContent += parseInt(element.textContent)
+      secondNumber =Number(display.textContent)
+    } else{
+      secondNumber= undefined;
+      firstNumber=undefined;
+    }
+   
     
   })
   
 })
 
-
-operators.forEach(function (op) {
-  op.addEventListener('click', ()=>{
-   display.textContent=secondNumber;
-  })
-
-})
 operators.forEach((ops)=>{
   ops.addEventListener('click',()=>{
+    display.textContent="";
     operator=ops.textContent;
+    operatorClicked= true
   })
 })
